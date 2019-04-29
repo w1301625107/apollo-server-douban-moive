7a45c4f469c5369c867d2620f99f599beea40874
wuchouchou <w1301625107@gmail.com>
2019-04-29 23:20:09 +0800

    feat: 添加注册，登陆 (与douban无关）

---
+++ src/fake_data/user.ts
@@ -0,0 +1,22 @@
+const users = [
+  {
+    id: 1,
+    email: 'fong@test.com',
+    password: '$2b$04$wcwaquqi5ea1Ho0aKwkZ0e51/RUkg6SGxaumo8fxzILDmcrv4OBIO', // 123456
+    name: 'Fong',
+  },
+  {
+    id: 2,
+    email: 'kevin@test.com',
+    passwrod: '$2b$04$uy73IdY9HVZrIENuLwZ3k./0azDvlChLyY1ht/73N4YfEZntgChbe', // 123456
+    name: 'Kevin',
+  },
+  {
+    id: 3,
+    email: 'mary@test.com',
+    password: '$2b$04$UmERaT7uP4hRqmlheiRHbOwGEhskNw05GHYucU73JRf8LgWaqWpTy', // 123456
+    name: 'Mary',
+  },
+]
+
+export default users
